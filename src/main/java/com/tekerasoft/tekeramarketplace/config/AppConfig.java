ddc2ea8b1d3747ddcb6567b7ff266d38627c3675
CodeWithBayramali <alidrkk07@gmail.com>
2025-06-17 14:23:55 +0300

    cors-config-add

--- src/main/java/com/tekerasoft/tekeramarketplace/config/AppConfig.java
+++ src/main/java/com/tekerasoft/tekeramarketplace/config/AppConfig.java
@@ -8,30 +8,29 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@Configuration
 public class AppConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        // Sadece güvenilir frontend origin'lerini burada tanımla
-        configuration.setAllowedOrigins(List.of(
-                "https://ar.tekera21.com",
-                "https://tekera21.com",
-                "https://arzuamber.com"
-        ));
-
-//        configuration.setAllowedOrigins(List.of("*"));
-
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(List.of("*")); // İzin verilen header'lar
-        configuration.setAllowCredentials(true); // Token, cookie gibi kimlik doğrulama verilerini taşıyabilmek için
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Tüm endpointler için uygula
-
-        return source;
-    }
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        // Sadece güvenilir frontend origin'lerini burada tanımla
+//        configuration.setAllowedOrigins(List.of(
+//                "https://ar.tekera21.com",
+//                "https://tekera21.com",
+//                "https://arzuamber.com"
+//        ));
+//
+////        configuration.setAllowedOrigins(List.of("*"));
+//
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+//        configuration.setAllowedHeaders(List.of("*")); // İzin verilen header'lar
+//        configuration.setAllowCredentials(true); // Token, cookie gibi kimlik doğrulama verilerini taşıyabilmek için
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration); // Tüm endpointler için uygula
+//
+//        return source;
+//    }
 
 }
---
+++ src/main/java/com/tekerasoft/tekeramarketplace/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.tekerasoft.tekeramarketplace.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://ar.tekera21.com",
+                        "https://tekera21.com",
+                        "https://arzuamber.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Bu satır kritik: Cookie/token gibi verilerin taşınmasına izin verir
+    }
+}
