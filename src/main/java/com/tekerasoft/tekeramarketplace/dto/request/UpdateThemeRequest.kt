c8e87d6ca30e501a6ca3803fd65720f9fa76968a
CodeWithBayramali <alidrkk07@gmail.com>
2025-07-03 18:06:48 +0300

    delete-theme-entity

--- src/main/java/com/tekerasoft/tekeramarketplace/dto/request/UpdateThemeRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.tekerasoft.tekeramarketplace.dto.request
-
-data class UpdateThemeRequest(
-
-)
--- src/main/java/com/tekerasoft/tekeramarketplace/repository/jparepository/ThemeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tekerasoft.tekeramarketplace.repository.jparepository;
-
-import com.tekerasoft.tekeramarketplace.model.entity.Theme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ThemeRepository extends JpaRepository<Theme, UUID> {
-}
