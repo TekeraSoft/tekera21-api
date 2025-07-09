f1d3e538b9e22aa0c56be72534eaabe37002cd0b
CodeWithBayramali <alidrkk07@gmail.com>
2025-07-09 20:37:28 +0300

    add-request-size

--- src/main/java/com/tekerasoft/tekeramarketplace/config/WebConfig.java
+++ src/main/java/com/tekerasoft/tekeramarketplace/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.tekerasoft.tekeramarketplace.config;
 
 
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +28,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("Content-Type","Authorization","token")
                 .allowCredentials(true); // Bu satır kritik: Cookie/token gibi verilerin taşınmasına izin verir
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(600));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(600));
+        return factory.createMultipartConfig();
     }
 }
